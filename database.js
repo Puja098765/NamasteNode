2ce9631e4b9e44b0a6b396d8ff0aa41879c7b00a
Puja <pujakeshri098765@gmail.com>
2025-07-28 18:02:35 +0530

    initial commit

--- database.js
+++ database.js
@@ -1,8 +1,48 @@
-// go to mongodb website
+
+const  {MongoClient}= require("mongodb");
+const url = "mongodb+srv://Puja098765:yuZhxdnFJbgZyNFh@namastenode.oyresk0.mongodb.net/";
+const client = new MongoClient(url);
+const dbName = "HelloWorld";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('User');
+
+//   insert
+const data={
+    firstname:"Madhu",
+    lastname:"Devi",
+    city:"Gaya",
+    phoneNumber:"65355358278",
+};
+const insertResult = await collection.insertMany([data]);
+console.log('Inserted documents =>', insertResult);
+
+//  Read
+const findResult = await collection.find({}).toArray();
+console.log('Found documents =>', findResult);
+
+// count
+const countResult = await collection.countDocuments({});
+console.log("Count of documents in the User collection =>", countResult);
+
+// Find all documents with a filter of firstname:Madhu
+const result = await collection.find({firstname:"Madhu"}).toArray();
+console.log("result =>", result);
+
+  return 'done.';
+}
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
+  // go to mongodb website
 // create a free M0 cluster 
 // create a user
 // get the connection string
 // install mongo DB compass
 // nodejs and MDB  connector= npm mongodb
-require("mongodb")
-const URI= "mongodb+srv://Puja098765:yuZhxdnFJbgZyNFh@namastenode.oyresk0.mongodb.net/";
